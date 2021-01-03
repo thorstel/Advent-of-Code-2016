efd2a0517412582b6056a4ab060532266cf25cb0
thorstel <thorstel@pm.me>
2021-01-03 14:40:22 +0100

    Solution for day 8.

---
+++ day08.cc
@@ -0,0 +1,255 @@
+// https://adventofcode.com/2016/day/8
+
+#include <bits/stdc++.h>
+
+#define all(x) begin(x), end(x)
+#define sz(x) ((int)(x).size())
+using namespace std;
+
+void solve(istream& ins)
+{
+	assert(ins.good());
+	auto rect_pat = regex {R"(rect (\d+)x(\d+))"};
+	auto rotr_pat = regex {R"(rotate row y=(\d+) by (\d+))"};
+	auto rotc_pat = regex {R"(rotate column x=(\d+) by (\d+))"};
+	auto screen = vector<vector<char>>(6, vector<char>(50, '.'));
+	for (string line; getline(ins, line);) {
+		smatch m;
+		if (regex_match(line, m, rect_pat)) {
+			int A = stoi(m[1]), B = stoi(m[2]);
+			for (int r = 0; r < B; ++r) {
+				for (int c = 0; c < A; ++c) {
+					screen[r][c] = '#';
+				}
+			}
+		} else if (regex_match(line, m, rotr_pat)) {
+			int A = stoi(m[1]), B = stoi(m[2]);
+			rotate(rbegin(screen[A]), rbegin(screen[A]) + B, rend(screen[A]));
+		} else if (regex_match(line, m, rotc_pat)) {
+			int A = stoi(m[1]), B = stoi(m[2]);
+			for (int _ = 0; _ < B; ++_) {
+				for (int i = 0; i < sz(screen) - 1; ++i) {
+					swap(screen[i][A], screen.back()[A]);
+				}
+			}
+		} else { assert(false); }
+	}
+	auto cnt_lit = [](int p, auto& row) { return p + count(all(row), '#'); };
+	cout << accumulate(all(screen), 0, cnt_lit) << '\n';
+	for (auto& row : screen) {
+		for (char c : row) { cout << c; }
+		cout << '\n';
+	}
+}
+
+////////////////////////////////////////////////////////////////////////
+//                            SETUP STUFF                             //
+////////////////////////////////////////////////////////////////////////
+
+static const string actual_input =
+R"""(rect 1x1
+rotate row y=0 by 7
+rect 1x1
+rotate row y=0 by 5
+rect 1x1
+rotate row y=0 by 5
+rect 1x1
+rotate row y=0 by 2
+rect 1x1
+rotate row y=0 by 3
+rect 1x1
+rotate row y=0 by 5
+rect 1x1
+rotate row y=0 by 3
+rect 1x1
+rotate row y=0 by 2
+rect 1x1
+rotate row y=0 by 3
+rect 2x1
+rotate row y=0 by 7
+rect 6x1
+rotate row y=0 by 3
+rect 2x1
+rotate row y=0 by 2
+rect 1x2
+rotate row y=1 by 10
+rotate row y=0 by 3
+rotate column x=0 by 1
+rect 2x1
+rotate column x=20 by 1
+rotate column x=15 by 1
+rotate column x=5 by 1
+rotate row y=1 by 5
+rotate row y=0 by 2
+rect 1x2
+rotate row y=0 by 5
+rotate column x=0 by 1
+rect 4x1
+rotate row y=2 by 15
+rotate row y=0 by 5
+rotate column x=0 by 1
+rect 4x1
+rotate row y=2 by 5
+rotate row y=0 by 5
+rotate column x=0 by 1
+rect 4x1
+rotate row y=2 by 10
+rotate row y=0 by 10
+rotate column x=8 by 1
+rotate column x=5 by 1
+rotate column x=0 by 1
+rect 9x1
+rotate column x=27 by 1
+rotate row y=0 by 5
+rotate column x=0 by 1
+rect 4x1
+rotate column x=42 by 1
+rotate column x=40 by 1
+rotate column x=22 by 1
+rotate column x=17 by 1
+rotate column x=12 by 1
+rotate column x=7 by 1
+rotate column x=2 by 1
+rotate row y=3 by 10
+rotate row y=2 by 5
+rotate row y=1 by 3
+rotate row y=0 by 10
+rect 1x4
+rotate column x=37 by 2
+rotate row y=3 by 18
+rotate row y=2 by 30
+rotate row y=1 by 7
+rotate row y=0 by 2
+rotate column x=13 by 3
+rotate column x=12 by 1
+rotate column x=10 by 1
+rotate column x=7 by 1
+rotate column x=6 by 3
+rotate column x=5 by 1
+rotate column x=3 by 3
+rotate column x=2 by 1
+rotate column x=0 by 1
+rect 14x1
+rotate column x=38 by 3
+rotate row y=3 by 12
+rotate row y=2 by 10
+rotate row y=0 by 10
+rotate column x=7 by 1
+rotate column x=5 by 1
+rotate column x=2 by 1
+rotate column x=0 by 1
+rect 9x1
+rotate row y=4 by 20
+rotate row y=3 by 25
+rotate row y=2 by 10
+rotate row y=0 by 15
+rotate column x=12 by 1
+rotate column x=10 by 1
+rotate column x=8 by 3
+rotate column x=7 by 1
+rotate column x=5 by 1
+rotate column x=3 by 3
+rotate column x=2 by 1
+rotate column x=0 by 1
+rect 14x1
+rotate column x=34 by 1
+rotate row y=1 by 45
+rotate column x=47 by 1
+rotate column x=42 by 1
+rotate column x=19 by 1
+rotate column x=9 by 2
+rotate row y=4 by 7
+rotate row y=3 by 20
+rotate row y=0 by 7
+rotate column x=5 by 1
+rotate column x=3 by 1
+rotate column x=2 by 1
+rotate column x=0 by 1
+rect 6x1
+rotate row y=4 by 8
+rotate row y=3 by 5
+rotate row y=1 by 5
+rotate column x=5 by 1
+rotate column x=4 by 1
+rotate column x=3 by 2
+rotate column x=2 by 1
+rotate column x=1 by 3
+rotate column x=0 by 1
+rect 6x1
+rotate column x=36 by 3
+rotate column x=25 by 3
+rotate column x=18 by 3
+rotate column x=11 by 3
+rotate column x=3 by 4
+rotate row y=4 by 5
+rotate row y=3 by 5
+rotate row y=2 by 8
+rotate row y=1 by 8
+rotate row y=0 by 3
+rotate column x=3 by 4
+rotate column x=0 by 4
+rect 4x4
+rotate row y=4 by 10
+rotate row y=3 by 20
+rotate row y=1 by 10
+rotate row y=0 by 10
+rotate column x=8 by 1
+rotate column x=7 by 1
+rotate column x=6 by 1
+rotate column x=5 by 1
+rotate column x=3 by 1
+rotate column x=2 by 1
+rotate column x=1 by 1
+rotate column x=0 by 1
+rect 9x1
+rotate row y=0 by 40
+rotate column x=44 by 1
+rotate column x=35 by 5
+rotate column x=18 by 5
+rotate column x=15 by 3
+rotate column x=10 by 5
+rotate row y=5 by 15
+rotate row y=4 by 10
+rotate row y=3 by 40
+rotate row y=2 by 20
+rotate row y=1 by 45
+rotate row y=0 by 35
+rotate column x=48 by 1
+rotate column x=47 by 5
+rotate column x=46 by 5
+rotate column x=45 by 1
+rotate column x=43 by 1
+rotate column x=40 by 1
+rotate column x=38 by 2
+rotate column x=37 by 3
+rotate column x=36 by 2
+rotate column x=32 by 2
+rotate column x=31 by 2
+rotate column x=28 by 1
+rotate column x=23 by 3
+rotate column x=22 by 3
+rotate column x=21 by 5
+rotate column x=20 by 1
+rotate column x=18 by 1
+rotate column x=17 by 3
+rotate column x=13 by 1
+rotate column x=10 by 1
+rotate column x=8 by 1
+rotate column x=7 by 5
+rotate column x=6 by 5
+rotate column x=5 by 1
+rotate column x=3 by 5
+rotate column x=2 by 5
+rotate column x=1 by 5)""";
+
+int main(int argc, const char *argv[])
+{
+	if (argc > 1) {
+		ifstream ifs {argv[1]};
+		solve(ifs);
+	} else {
+		auto iss = istringstream {actual_input};
+		solve(iss);
+	}
+	return 0;
+}
